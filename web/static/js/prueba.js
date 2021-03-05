ef2d3b8e5e9839cb30d414d108ea77b3d25971c1
Christian Rojas <christian.rojas@utec.edu.pe>
2021-03-05 17:41:46 -0500

    carpeta estatica

---
+++ web/static/js/prueba.js
@@ -0,0 +1,9 @@
+/*$(document).ready(() => {
+  $('#parrafoo').text('este es otro parrafoo')
+})*/
+
+$(document).ready(() => {
+  $('.btn-menu').click(() => {
+    $('.item').toggleClass("show");
+  });
+});
